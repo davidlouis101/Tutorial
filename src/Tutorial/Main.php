6760addacb0e76b08794e7cc11165b1ce2a9acbe
xXNiceYT <23722462+xXNiceYT@users.noreply.github.com>
2019-07-05 00:10:42 -0400

    Added Commands

---
+++ src/Tutorial/Main.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tutorial;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat as C;
+
+class Main extends PluginBase{
+
+	public function onLoad(): void{
+		$this->getLogger()->info(C::GREEN . "Loading Tutorial");
+	}
+
+    public function onEnable(): void{
+        $this->getLogger()->info(C::YELLOW . "Enabled Tutorial");
+    }
+
+    public function onDisable(): void{
+        $this->getLogger()->info(C::RED . "Disabled Tutorial");
+    }
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool{
+	    //$sender is a player
+        switch($command){
+            case "test":
+            $sender->sendMessage("BABY GURL XD");
+            break;
+            case "lul":
+            $sender->sendMessage("BABY GURLLLLLLLLLLLLLLLLLLLL");
+            break;
+        }
+        return true;
+    }
+}
